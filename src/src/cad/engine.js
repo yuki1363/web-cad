20834c4b839c28f7f8b6f15fae23ad0b96c93a53
yuki1363 <yuki.yukitkmt0@gmail.com>
2026-03-12 12:41:43 +0900

    Add shape selection functionality to engine

--- src/src/cad/engine.js
+++ src/src/cad/engine.js
@@ -1,19 +1,23 @@
 export const shapes = []
 
+let selectedIndex = -1
+
 export function startEngine(){
-
  console.log("CAD Engine Started")
-
 }
 
 export function addShape(shape){
-
  shapes.push(shape)
-
 }
 
 export function getShapes(){
-
  return shapes
+}
 
+export function selectShape(index){
+ selectedIndex = index
+}
+
+export function getSelected(){
+ return selectedIndex
 }
