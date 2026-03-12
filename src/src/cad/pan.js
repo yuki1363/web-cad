7a4a37d3e2bdaaf1c28ba832a0ad0873c07e0a0f
yuki1363 <yuki.yukitkmt0@gmail.com>
2026-03-12 12:54:46 +0900

    Implement panning functionality for canvas

--- src/src/cad/pan.js
+++ src/src/cad/pan.js
@@ -1,1 +1,27 @@
+let offsetX=0
+let offsetY=0
 
+let dragging=false
+
+export function initPan(canvas){
+
+canvas.addEventListener("mousedown",()=>dragging=true)
+
+canvas.addEventListener("mouseup",()=>dragging=false)
+
+canvas.addEventListener("mousemove",(e)=>{
+
+if(!dragging)return
+
+offsetX+=e.movementX
+offsetY+=e.movementY
+
+})
+
+}
+
+export function getPan(){
+
+return {offsetX,offsetY}
+
+}
