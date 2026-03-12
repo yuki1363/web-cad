b660e4fa75aa399620c62ef60f05819676c4a737
yuki1363 <yuki.yukitkmt0@gmail.com>
2026-03-12 12:58:23 +0900

    Update history.js

--- src/src/cad/history.js
+++ src/src/cad/history.js
@@ -1,1 +1,28 @@
+let undoStack=[]
+let redoStack=[]
 
+export function saveState(state){
+
+undoStack.push(JSON.stringify(state))
+
+}
+
+export function undo(shapes){
+
+if(undoStack.length===0)return
+
+redoStack.push(JSON.stringify(shapes))
+
+return JSON.parse(undoStack.pop())
+
+}
+
+export function redo(shapes){
+
+if(redoStack.length===0)return
+
+undoStack.push(JSON.stringify(shapes))
+
+return JSON.parse(redoStack.pop())
+
+}
