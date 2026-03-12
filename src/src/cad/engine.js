ebfadae42c775e3efb4e8d43e5c0d9bc23954531
yuki1363 <yuki.yukitkmt0@gmail.com>
2026-03-12 12:39:00 +0900

    Add shape management functions to engine.js

--- src/src/cad/engine.js
+++ src/src/cad/engine.js
@@ -1,5 +1,19 @@
+export const shapes = []
+
 export function startEngine(){
 
  console.log("CAD Engine Started")
+
+}
+
+export function addShape(shape){
+
+ shapes.push(shape)
+
+}
+
+export function getShapes(){
+
+ return shapes
 
 }
